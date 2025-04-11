f89a1a44eff809ddec442a029b97c24f0144cb42
KN Creations <kirannandi896@gmail.com>
2025-04-11 17:20:02 +0530

    init2

--- ai_mock_interviews/app/(root)/page.tsx
+++ ai_mock_interviews/app/(root)/page.tsx
@@ -13,9 +13,35 @@ import {
 async function Home() {
   const user = await getCurrentUser();
 
+  // If user is not logged in, only show the CTA section
+  if (!user) {
+    return (
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice real interview questions & get instant feedback
+          </p>
+
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/sign-in">Sign in to Start</Link>
+          </Button>
+        </div>
+
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+    );
+  }
+
   const [userInterviews, allInterview] = await Promise.all([
-    getInterviewsByUserId(user?.id!),
-    getLatestInterviews({ userId: user?.id! }),
+    getInterviewsByUserId(user.id),
+    getLatestInterviews({ userId: user.id }),
   ]);
 
   const hasPastInterviews = userInterviews?.length! > 0;
@@ -52,7 +78,7 @@ async function Home() {
             userInterviews?.map((interview) => (
               <InterviewCard
                 key={interview.id}
-                userId={user?.id}
+                userId={user.id}
                 interviewId={interview.id}
                 role={interview.role}
                 type={interview.type}
@@ -74,7 +100,7 @@ async function Home() {
             allInterview?.map((interview) => (
               <InterviewCard
                 key={interview.id}
-                userId={user?.id}
+                userId={user.id}
                 interviewId={interview.id}
                 role={interview.role}
                 type={interview.type}
